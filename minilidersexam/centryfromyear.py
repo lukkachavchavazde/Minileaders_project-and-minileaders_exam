3eef5411702ae32468d3843cdbaee00d56dc5f9d
lukkachavchavazde <lukachavchavadze646@gmail.com>
2024-10-27 19:35:26 +0400

    exam

---
+++ minilidersexam/centryfromyear.py
@@ -0,0 +1,201 @@
+def century(year):
+    if  1 < year <= 100: 
+        return 1
+    if 100 < year <=200:
+        return 2
+    if 200 < year <=300:
+        return 3
+    if 300 < year <=400:
+        return 4
+    if 400 < year <=500: 
+        return 5
+    if 500 < year <=600:
+        return 6
+    if 600 < year <=700:
+        return 7
+    if 700 < year <=800:
+        return 8
+    if 800 < year <= 900: 
+        return 9
+    if 900 < year <=1000:
+        return 10
+    if 1000 < year <=1100:
+        return 11
+    if 1100 < year <=1200:
+        return 12
+    if 1200 < year <=1300:
+        return 13
+    if 1300 < year <=1400:
+        return 14
+    if 1400 < year <=1500:
+        return 15
+    if 1500 < year <=1600:
+        return 16
+    if 1600 < year <=1700:
+        return 17
+    if 1700 < year <=1800:
+        return 18
+    if 1800 < year <=1900:
+        return 19
+    if 1900 < year <=2000:
+        return 20
+    if 2000 < year <=2100:
+        return 21
+    if 2100 < year <=2200:
+        return 22
+    if 2200 < year <=2300:
+        return 23
+    if 2300 < year <=2400:
+        return 24
+    if 2400 < year <=2500:
+        return 25
+    if 2500 < year <=2600:
+        return 26
+    if 2600 < year <=2700:
+        return 27
+    if 2700 < year <=2800:
+        return 28
+    if 2800 < year <=2900:
+        return 29
+    if 2900 < year <=3000:
+        return 30
+    if 3000 < year <=3100:
+        return 31
+    if 3100 < year <=3200:
+        return 32
+    if 3200 < year <=3300:
+        return 33
+    if 3300 < year <=3400:
+        return 34
+    if 3400 < year <= 3500:
+        return 35
+    if 3500 < year <=3600:
+        return 36
+    if 3600 < year <=3700:
+        return 37
+    if 3700 < year <=3800:
+        return 38
+    if 3800 < year <=3900:
+        return 39
+    if 3900 < year <=4000:
+        return 40
+    if 4000 < year <=4100:
+        return 41
+    if 4100 < year <= 4200:
+        return 42
+    if  4200 < year <= 4300: 
+        return 43 
+    if 4300< year <=4400:
+        return 44
+    if 4400 < year <=4500:
+        return 45
+    if 4500 < year <=4600:
+        return 46
+    if 4600 < year <=4700: 
+        return 47
+    if 4700 < year <=4800:
+        return 48
+    if 4800 < year <=4900:
+        return 49
+    if 4900 < year <=5000:
+        return 50
+    if 5000 < year <= 5100: 
+        return 51
+    if 5100 < year <=5200:
+        return 52
+    if 5200 < year <=5300:
+        return 53
+    if 5300 < year <=5400:
+        return 54
+    if 5400 < year <=5500:
+        return 55
+    if 5500 < year <=5600:
+        return 56
+    if 5600 < year <=5700:
+        return 57
+    if 5700 < year <=5800:
+        return 58
+    if 5800 < year <=5900:
+        return 59
+    if 5900 < year <=6000:
+        return 60
+    if 6000 < year <=6100:
+        return 61
+    if 6100 < year <=6200:
+        return 62
+    if 6200 < year <=6300:
+        return 63
+    if 6300 < year <= 6400:
+        return 64
+    if 6400 < year <=6500:
+        return 65
+    if 6500 < year <=6600:
+        return 66
+    if 6600 < year <=6700:
+        return 67
+    if 6700 < year <=6800:
+        return 68
+    if 6800 < year <=6900:
+        return 69
+    if 6900 < year <=7000:
+        return 70
+    if 7000 < year <=7100:
+        return 71
+    if 7100 < year <=7200:
+        return 72
+    if 7200 < year <=7300:
+        return 73
+    if 7300 < year <=7400:
+        return 74
+    if 7400 < year <=7500:
+        return 75
+    if 7500 < year <=7600:
+        return 76
+    if 7600 < year <= 7700:
+        return 77
+    if 7700 < year <=7800:
+        return 78
+    if 7800 < year <=7900:
+        return 79
+    if 7900 < year <=8000:
+        return 80
+    if 8000 < year <=8100:
+        return 81
+    if 8100 < year <=8200:
+        return 82
+    if 8200 < year <=8300:
+        return 83
+    if 8300 < year <= 8400:
+        return 84
+    if 8400 < year <=8500:
+        return 85
+    if 8500 < year <=8600:
+        return 86
+    if  8600 < year <=8700:
+        return 87
+    if 8700 < year <=8800:
+        return 88
+    if 8800 < year <= 8900:
+        return 89
+    if 8900 < year <=9000:
+        return 90
+    if 9000 < year <=9100:
+        return 91
+    if 9100 < year <=9200:
+        return 92
+    if 9200 < year <=9300:
+        return 93
+    if 9300 < year <=9400:
+        return 94
+    if 9400 < year <=9500:
+        return 95
+    if 9500 < year <=9600:
+        return 96
+    if 9600 < year <=9700:
+        return 97
+    if 9700 < year <=9800:
+        return 98
+    if 9800 < year <=9900:
+        return 99
+    if 9900< year <=10000:
+        return 100
